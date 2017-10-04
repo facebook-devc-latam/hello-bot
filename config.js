704dffbb4c45a49d70ef9f2712a5672cad59fa3d
Kike Rios <web.lrios@gmail.com>
2017-10-03 23:10:03 -0600

    add/use configuration file

---
+++ config.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const _ = require('lodash')
+const Confidence = require('confidence')
+const ToBoolean = require('to-boolean')
+const env = require('dotenv').config()
+
+const config = {
+    $meta: 'This file defines all configuration for project.',
+    projectName: 'hello-bot',
+    app: {
+      host: process.env.APP_HOST || 'localhost',
+      port: process.env.APP_PORT || 3500
+    },
+    logger: {
+        options: {
+            console: ToBoolean(_.defaultTo(process.env.LOGGER_DEBUG, true))
+        }
+    }
+}
+
+const store = new Confidence.Store(config)
+const criteria = {
+  env: process.env.APP_ENV
+}
+
+module.exports = {
+  get: key => store.get(key, criteria),
+  meta: key => store.meta(key, criteria)
+}
--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-'use strict';
+'use strict'
 
-const Hapi = require('hapi');
+const Hapi = require('hapi')
+const Config = require('./config')
+const Logger = require('bucker').createLogger({
+  name: 'server',
+  console: Config.get('/logger/options/console')
+})
 
 // Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
+const server = new Hapi.Server()
+server.connection({
+    host: Config.get('/app/host'),
+    port: Config.get('/app/port')
+})
 
 // Add the route
 server.route({
     method: 'GET',
-    path:'/', 
+    path:'/',
     handler: function (request, reply) {
 
-        return reply('hello bot');
+        return reply('hello bot')
     }
-});
+})
 
 // Start the server
 server.start((err) => {
 
     if (err) {
-        throw err;
+        throw err
     }
-    console.log('Server running at:', server.info.uri);
-});
+    Logger.info('Server running at:', server.info.uri)
+})
